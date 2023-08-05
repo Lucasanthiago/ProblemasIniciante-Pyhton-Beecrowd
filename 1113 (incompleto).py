420614f854f13d0beded6a16cc072a52ba833db7
Lucasanthiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-08-05 19:05:25 -0300

    Update 1113 (incompleto).py

--- 1113 (incompleto).py
+++ 1113 (incompleto).py
@@ -1,1 +1,13 @@
 
+
+while True:
+    x1, x2 = input().split()
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x1 > x2:
+        print("Decrescente")
+    if x1 < x2:
+        print("Crescente")
+    if x1 == x2:
+        break
