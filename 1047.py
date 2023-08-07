24af3e264e6868387c80c5d358af8450a3b291eb
Lucasanthiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-08-07 20:39:08 -0300

    Update 1047.py

--- 1047.py
+++ 1047.py
@@ -1,1 +1,21 @@
+hora_ini, min_ini,hora_fin,min_fin = input().split()
 
+hora_ini = int(hora_ini)
+min_ini = int(min_ini)
+hora_fin = int(hora_fin)
+min_fin = int(min_fin)
+
+hora = hora_fin - hora_ini
+min = min_fin - min_ini
+
+if min < 0:
+    min = min + 60
+    hora = hora - 1
+
+if hora < 0:
+    hora = hora + 24
+
+if hora_fin == hora_ini and min_ini == min_fin:
+    hora = 24
+
+print("O JOGO DUROU %d HORA(S) E %d MINUTO(S)" %(hora,min))
