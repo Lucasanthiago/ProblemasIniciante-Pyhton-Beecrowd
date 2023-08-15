9157b07b0d64bfe5c98f829b58f12d4ce5caa295
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-08-15 15:08:37 -0300

    Update 1116.py

--- 1116.py
+++ 1116.py
@@ -1,1 +1,18 @@
 
+
+num = int(input())
+
+while num > 0:
+
+    x1, x2 = input().split()
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x2 == 0:
+        div = "divisao impossivel"
+    else:
+        div = x1 / x2
+
+    num = num - 1
+
+    print(div)
