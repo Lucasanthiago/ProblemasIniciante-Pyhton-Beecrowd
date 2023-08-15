048cda68656f5b3a9b612b3bd6067d3cdbc0686a
Lucasanthiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-08-14 21:37:47 -0300

    Update 1115.py

--- 1115.py
+++ 1115.py
@@ -1,1 +1,23 @@
 
+while True:
+    x1, x2 = input().split()
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x1 > 0:
+        if x2 > 0:
+            print("primeiro")
+            continue
+        if x2 < 0:
+            print("quarto")
+            continue
+
+    if x1 < 0:
+        if x2 > 0:
+            print("segundo")
+            continue
+        if x2 < 0:
+            print("terceiro")
+            continue
+
+
