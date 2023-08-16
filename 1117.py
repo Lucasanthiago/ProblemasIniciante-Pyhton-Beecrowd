c3c791db028467731a8b18e7b5c6b532c71ce935
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-08-16 16:18:09 -0300

    Update 1117.py

--- 1117.py
+++ 1117.py
@@ -1,1 +1,22 @@
 
+nota_1 = -1
+nota_2 = -1
+
+while (nota_1 and nota_2 < 0):
+    nota_temp = (float(input()))
+
+
+    if nota_temp >= 0 and nota_temp <= 10:
+        if nota_1 < 0:
+            nota_1 = nota_temp
+            continue
+        if nota_1 > 0:
+            nota_2 = nota_temp
+            continue
+        continue
+    else:
+        print("nota invalida")
+        continue
+
+med = (nota_1 + nota_2) / 2
+print("media =", med)
