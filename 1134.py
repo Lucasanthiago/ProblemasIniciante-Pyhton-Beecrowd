52f3a89855b65524be7c888dc6e9de713b03de96
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-09-19 16:19:19 -0300

    Update 1134.py

--- 1134.py
+++ 1134.py
@@ -1,1 +1,20 @@
+Alcool = 0
+Gasolina = 0
+Diesel = 0
+x = 0
+
+while x != 4:
+    x = int(input())
+
+    if x == 1:
+        Alcool = Alcool + 1
+    if x == 2:
+        Gasolina = Gasolina + 1
+    if x == 3:
+        Diesel = Diesel + 1
+
+print("MUITO OBRIGADO\n"
+      "Alcool: %d\n"
+      "Gasolina: %d\n"
+      "Diesel: %d" % (Alcool,Gasolina,Diesel))
 
