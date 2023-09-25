fb498718c2b31b082eba2fbe40976a793838c865
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-09-25 02:40:52 -0300

    Update 1145.py

--- 1145.py
+++ 1145.py
@@ -1,1 +1,19 @@
 
+x,y = input().split()
+
+x = int(x)
+backup_x = x
+y = int(y)
+num = 0
+inter = (y/x)
+
+while inter > 0:
+    lista = []
+    while x > 0:
+        num += 1
+        lista.append(num)
+        x -= 1
+    lst_new = [str(a) for a in lista]
+    print(" ".join(lst_new))
+    x = backup_x
+    inter -= 1
