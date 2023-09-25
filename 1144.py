7b68d74c1db0fcfab98cc7fb745b04422e5c965c
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-09-24 22:25:40 -0300

    Update 1144.py

--- 1144.py
+++ 1144.py
@@ -1,1 +1,15 @@
 
+num = int(input())
+init = 1
+num = num 
+
+while num > 0:
+    num -= 1
+
+    init2 = init ** 2
+    init3 = init ** 3
+
+    print("%d %d %d" %(init, init2, init3))
+    print("%d %d %d" %(init, init2+1, init3+1))
+
+    init += 1
