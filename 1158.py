5daed0f887565dc4ebdbe794808088d24b3f2020
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-10-18 06:18:59 -0300

    Update 1158.py

--- 1158.py
+++ 1158.py
@@ -1,1 +1,18 @@
 
+N = int(input())
+
+for x in range(N):
+    aux = 0
+    lista = []
+    x1, x2 = input().split()
+    x1 = int(x1)
+    x2 = int(x2)
+
+    if x1 % 2 == 0:
+        x1 += 1
+
+    while aux < x2:
+        lista.append(x1)
+        aux += 1
+        x1 += 2
+    print(sum(lista))
