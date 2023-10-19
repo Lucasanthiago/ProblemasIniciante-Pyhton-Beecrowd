a97f2a2f89408783a2471c7a7d3aa1027028500a
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-10-19 13:58:24 -0300

    Update 1172.py

--- 1172.py
+++ 1172.py
@@ -1,1 +1,13 @@
+vet = []
 
+
+for x in range(10):
+    num = int(input())
+    vet.append(num)
+
+for i in range(10):
+    if vet[i] < 1:
+        vet[i] = 1
+
+for j in range(10):
+    print("X[%d] = %d" %(j, vet[j]))
