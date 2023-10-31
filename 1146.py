bd9e752248ecfebf4fecfd277711f4b0e23d372c
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2023-10-31 09:20:19 -0300

    Update 1146.py

--- 1146.py
+++ 1146.py
@@ -1,1 +1,13 @@
 
+x = 1
+
+while x != 0:
+    x = int(input())
+    if x == 0:
+        break
+    lits = []
+
+    for i in range(x):
+        lits.append(i+1)
+
+    print(' '.join(map(str, lits)))
