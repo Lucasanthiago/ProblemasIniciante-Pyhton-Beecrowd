8e97876aa35baef803dd6a2589fe671dae4fbd00
Lucas Santhiago <88341941+Lucasanthiago@users.noreply.github.com>
2024-03-11 07:47:17 -0300

    Update 1177.py

--- 1177.py
+++ 1177.py
@@ -1,1 +1,18 @@
 
+
+
+var = int(input())
+
+x = 0
+num = 0
+
+while x < 1000:
+
+    while num < var:
+        print("N[%d] = %d" %(x, num))
+        num += 1
+        x += 1
+        if x == 1000:
+            break
+
+    num = 0
